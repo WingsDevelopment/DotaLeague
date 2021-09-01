2d67285224261fdc157b955b351e9569f282aedd
WingsDevelopment <bans.cowboy@gmail.com>
2021-09-01 17:12:03 +0200

    xx

--- DotaLeague/ApplicationServices/Services/PlayerService.cs
+++ DotaLeague/ApplicationServices/Services/PlayerService.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Utils.Exceptions;
 
-namespace ApplicationServices
+namespace ApplicationServices.Services
 {
     public class PlayerService : IPlayerService
     {
--- DotaLeague/ApplicationServices/Services/ReportService.cs
+++ DotaLeague/ApplicationServices/Services/ReportService.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApplicationServices
+namespace ApplicationServices.Services
 {
     public class ReportService
     {
